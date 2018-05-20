b1a318636690e8789219a9bfa5ce69db5a225a8b
pharaoh1995 <1362475541@qq.com>
2018-05-20 14:56:54 +0800

    init commit

---
+++ src/main/java/top/kisara/springbootcli/PharaohSpringbootCliApplication.java
@@ -0,0 +1,12 @@
+package top.kisara.springbootcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharaohSpringbootCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharaohSpringbootCliApplication.class, args);
+    }
+}
